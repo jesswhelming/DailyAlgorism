81bd719a02f557bbcc804876c5fef90116ff1d18
jess <jeesu29@gmail.com>
2020-09-19 01:17:22 +0900

    테스트 클래스 생성

---
+++ src/com/jesswhelming/dailyAlgorism/fullSearch/FullSearchTest.java
@@ -0,0 +1,10 @@
+package com.jesswhelming.dailyAlgorism.fullSearch;
+
+public class FullSearchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/jesswhelming/dailyAlgorism/stackNqueue/StackTest.java
@@ -0,0 +1,5 @@
+package com.jesswhelming.dailyAlgorism.stackNqueue;
+
+public class StackTest {
+
+}
