68b04ccff8ee3ae1094ef3b3da721c24724c88de
jess <jeesu29@gmail.com>
2020-09-27 12:09:19 +0900

    스택_프로그래머스_1st

---
+++ src/com/jesswhelming/dailyAlgorism/heap/PG_1st_YJS.java
@@ -0,0 +1,38 @@
+package com.jesswhelming.dailyAlgorism.heap;
+
+import java.util.Arrays;
+
+public class PG_1st_YJS {
+
+	public static void main(String[] args) {
+		
+		int[] prices = {1, 2, 3, 2, 3};
+		
+		solution(prices);
+
+	}
+	
+	public static int[] solution(int[] prices) {
+        int[] answer = new int [prices.length];
+        Arrays.fill(answer, 0);
+        
+        for(int i=0; i<prices.length; i++) {
+        	for(int j=i+1; j<prices.length; j++) {
+        		if(prices[i] <= prices[j]) {
+        			answer[i] += 1;
+        		}else {
+        			answer[i] += 1;
+        			break;
+        		}
+        	}
+        }
+        //System.out.println(answer);
+        for(int i=0; i<answer.length; i++) {
+        	System.out.println(answer[i]);
+        }
+        return answer;
+    }
+
+}
+
+
