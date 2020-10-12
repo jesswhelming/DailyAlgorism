b5d2f7bdcb8f70a7f67481c91c44c0316a599823
jess29 <jeesu29@gmail.com>
2020-10-12 18:53:16 +0900

    no message

---
+++ src/com/jesswhelming/dailyAlgorism/fullSearch/PG_fullSearch_2nd_YJS.java
@@ -0,0 +1,19 @@
+package com.jesswhelming.dailyAlgorism.fullSearch;
+
+public class PG_fullSearch_2nd_YJS {
+	public static void main(String[] args) {
+		String numbers = "011";
+		
+		solution(numbers);
+		
+	}
+	
+	public static int solution(String numbers) {
+        int answer = 0;
+        
+        
+        
+        
+        return answer;
+    }
+}
