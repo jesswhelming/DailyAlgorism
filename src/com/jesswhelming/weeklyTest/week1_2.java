149fd081fb89311582827b9e9d856abff6155177
jess29 <jeesu29@gmail.com>
2020-12-14 17:52:57 +0900

    no message

---
+++ src/com/jesswhelming/weeklyTest/week1_2.java
@@ -0,0 +1,22 @@
+package com.jesswhelming.weeklyTest;
+
+public class week1_2 {
+
+	public static void main(String[] args) {
+
+		String s = "()))((()";
+		
+		
+		System.out.println(solution(s));
+	}
+	
+	
+    public static String solution(String s) {
+        String answer = "";
+        
+        
+        return answer;
+    }
+	
+
+}
