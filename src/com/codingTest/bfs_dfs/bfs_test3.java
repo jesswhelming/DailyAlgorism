57ceb16315b84e884252150d57f6ed98c5d458b0
jess29 <jeesu29@gmail.com>
2021-03-29 09:41:53 +0900

    no message

--- src/com/codingTest/bfs_dfs/bfs_test3.java
+++ src/com/codingTest/bfs_dfs/bfs_test3.java
@@ -3,9 +3,7 @@ package com.codingTest.bfs_dfs;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class bfs_test2 {
-	int [][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-	int m, n = 0;
+public class bfs_test3 {
 	public static void main(String[] args) {
 		char[][] grid = {
 				{'1','1','0','0','1'},
@@ -13,21 +11,17 @@ public class bfs_test2 {
 				{'0','0','0','0','0'},
 				{'0','0','0','1','1'}
 									};
-		bfs_test2 a = new bfs_test2();
+		bfs_test3 a = new bfs_test3();
 		System.out.println(a.bfs(grid));
-		
-
 	}
-
+	int m, n = 0;
+	int [][] dirs = {{0,1}, {1,0}, {0,-1},{-1,0}};
 	private int bfs(char[][] grid) {
-		if(grid == null || grid.length == 0)
-			return 0;
-		
 		int count = 0;
 		
 		m = grid.length;
 		n = grid[0].length;
-		for(int i=0;i<m; i++){
+		for(int i=0; i<m;i++){
 			for(int j=0; j<n; j++){
 				if(grid[i][j] == '1'){
 					count ++;
@@ -35,12 +29,13 @@ public class bfs_test2 {
 				}
 			}
 		}
+		
+		
 		return count;
 	}
-
 	private void bfs(char[][] grid, int x, int y) {
 		Queue<int []> queue = new LinkedList<>();
-		queue.offer(new int[]{x, y});
+		queue.offer(new int[] {x, y});
 		
 		while(!queue.isEmpty()){
 			int[] point = queue.poll();
@@ -48,15 +43,13 @@ public class bfs_test2 {
 			for(int[] dir : dirs){
 				int x1 = point[0] + dir[0];
 				int y1 = point[1] + dir[1];
-
-				if(x1 >= 0 && y1 >= 0 && m > x1 && n > y1 && grid[x1][y1] == '1'){
+				
+				if(x1 >= 0 && y1 >= 0 && x1 < m && y1 < n && grid[x1][y1] == '1'){
 					grid[x1][y1] = '0';
-					queue.offer(new int[]{x1, y1});
+					queue.offer(new int[] {x1, y1});
 				}
 			}
-			
 		}
 		
 	}
-
 }
